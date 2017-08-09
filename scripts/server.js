84550b84fa79d89f5ba900df42d14b836ce2f9e9
Michael Angerman <stormasm@gmail.com>
2017-08-08 17:11:16 -0700

    scripts/server.js is now working

---
+++ scripts/server.js
@@ -0,0 +1,27 @@
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BABEL_ENV = 'development';
+process.env.NODE_ENV = 'development';
+
+
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+var config = require('./../config/webpack.config.dev');
+
+var app = new require('express')();
+var port = 3000;
+
+var compiler = webpack(config);
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+app.use(webpackHotMiddleware(compiler));
+app.use(function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(port, function(error) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
+  }
+});
